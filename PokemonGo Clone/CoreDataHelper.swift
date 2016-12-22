162fc551fb6512d80610df0c64717af947a494ee
ElMehdiMobi <elmehdimobi@gmail.com>
2016-12-22 17:05:28 +0100

    first commit

---
+++ PokemonGo Clone/CoreDataHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataHelper.swift
+//  PokemonGo Clone
+//
+//  Created by HUSTLER on 22/12/2016.
+//  Copyright © 2016 HUSTLER. All rights reserved.
+//
+
+import Foundation
---
+++ PokemonGo Clone/PokemonAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonAnnotation.swift
+//  PokemonGo Clone
+//
+//  Created by HUSTLER on 22/12/2016.
+//  Copyright © 2016 HUSTLER. All rights reserved.
+//
+
+import UIKit
+
+class PokemonAnnotation: MKAnnotation {
+
+}
